40bd05fd2bb1dc29089af67b5720efba297ed020
Saroj Rout <saroj_rout@intuit.com>
2018-01-12 01:45:07 -0800

    initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDXfU9Ny-ca1k810K4udSZETrSmKRwZ6CQ",
+    authDomain: "socialchat-b4d88.firebaseapp.com",
+    databaseURL: "https://socialchat-b4d88.firebaseio.com",
+    projectId: "socialchat-b4d88",
+    storageBucket: "",
+    messagingSenderId: "555058070701"
+  }
 };
